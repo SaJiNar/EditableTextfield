db1c80a9372849af50b12b38def28334b299d7b4
SaJiNar <sarajnares@gmail.com>
2023-05-16 17:22:27 +0200

    creating more EditText and adding them to java class.

--- app/src/main/java/com/example/camposdetexto/ActividadPrincipal.java
+++ app/src/main/java/com/example/camposdetexto/ActividadPrincipal.java
@@ -1,10 +1,13 @@
 package com.example.camposdetexto;
 
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 //Tutorial from: https://www.develou.com/edittext-android/
 
@@ -13,10 +16,24 @@ public class ActividadPrincipal extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.actividad_principal);
+
+        if (findViewById(R.id.campo_1).isFocusable()) {
+            findViewById(R.id.campo_2).requestFocus();
+        }
+
+        EditText campo1 = (EditText) findViewById(R.id.campo_cursor);
+        Log.d("Posicion cursor:", String.valueOf(campo1.getSelectionEnd()));
     }
 
     public void verValor(View view) {
         EditText campoTexto = (EditText) findViewById(R.id.campo_texto);
         Log.d("Valor ET", campoTexto.getText().toString());
+    }
+
+    //Ocultar teclado virtual
+    private void ocultarTeclado(TextView v) {
+        InputMethodManager imm =
+                (InputMethodManager) getSystemService(INPUT_METHOD_SERVICE);
+        imm.hideSoftInputFromWindow(v.getWindowToken(), 0);
     }
 }
